a31627d94005900790690520630fb5e49c6f9b59
dev <devluhar26@gmail.com>
2024-11-07 23:09:38 +0000

    .

--- instrument_list.py
+++ instrument_list.py
@@ -6,17 +6,22 @@ url = "https://demo.trading212.com/api/v0/equity/metadata/instruments"
 headers = {"Authorization": "20155216ZpCEwlxRBQEXFJzupJKWZkYRIQWnu"}
 response = requests.get(url, headers=headers)
 data = response.json()
+selected_isin = []
+
 for desired_stock in all_stock:
     ticker = yf.Ticker(desired_stock)
     isin = ticker.isin
     print(desired_stock)
     print(isin)
-    selected_isin=[]
     if isin!="-":
         for x in data:
             if (x["shortName"]==desired_stock) :
                 if x["isin"]==isin:
                     print(x)
+                    selected_isin.append(x["ticker"])
+                else:
+                    print(x)
+                    #add some function here to select one of the options
     else:
         temp=[]
         for x in data:
@@ -26,10 +31,15 @@ for desired_stock in all_stock:
         if temp!=[]:
             if len(temp)==1:
                 print(temp[0])
+                selected_isin.append(temp[0]["ticker"])
+
             else:
                 selected=input("which stock would you like to select?")
                 print(temp[int(selected)])
+                selected_isin.append(temp[int(selected)]["ticker"])
         else:
             print(desired_stock," not found in the system")
+            selected_isin.append("")
             pass
 
+print(selected_isin)
